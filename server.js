8530a85ac7c52ca5b263f1eb958736137e4ffb7e
catdog990 <ozmcastro@gmail.com>
2018-02-09 22:02:21 -0500

    added screenshot

--- server.js
+++ server.js
@@ -15,18 +15,8 @@ app.use(bodyParser.json());
 require('./App/Routing/htmlRoutes.js')(app);
 require('./App/Routing/apiRoutes.js')(app);
 
-//***********Just in Case I can't get the Requiring right***/////
-// app.get("/", function(req, res) {
-//   res.sendFile(path.join(__dirname, "./App/Public/home.html"));
-// });
-
-// app.get("/survey", function(req, res) {
-//   res.sendFile(path.join(__dirname, "./App/Public/survey.html"));
-// });
-
-
-
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
+
